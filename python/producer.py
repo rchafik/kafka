07e1dbb54308d18621f497650a815ced4f0aa42e
Rodrigo Chafik Choueiri <rchafik@gmail.com>
2025-06-16 20:34:10 +0000

    adicionando códigos python com o JT

--- python/producer.py
+++ python/producer.py
@@ -4,28 +4,30 @@ import json
 import os
 import logging
 
-def start_kafka_producer():
+def load_properties(file_path):
+    props = {}
+    with open(file_path, "r") as f:
+        for line in f:
+            line = line.strip()
+            if line and not line.startswith("#"):
+                key, value = line.split("=", 1)
+                key = key.strip()
+                value = value.strip()
 
-  username=os.environ['SASL_USERNAME']
-  password=os.environ['SASL_PASSWORD']
-  endpoint_stream="cell-1.streaming.sa-saopaulo-1.oci.oraclecloud.com:9092"
-  
-         
-  conf = {
-    'bootstrap.servers': os.environ.get('BOOTSTRAP_SERVERS', endpoint_stream),
-    'security.protocol': os.environ.get('SECURITY_PROTOCOL', 'SASL_SSL'),
-    'sasl.mechanism': os.environ.get('SASL_MECHANISM', 'PLAIN'),
-    'linger.ms': int(os.environ.get('LINGER_MS', 10)),
-    'batch.size': int(os.environ.get('BATCH_SIZE', (15 * (1024 * 1024) ))),
-    'acks':-1,
-    "queue.buffering.max.messages": 10000000, 
-    'compression.type': "lz4",
-    'max.in.flight.requests.per.connection': 1,
-    'sasl.username': username,
-    'sasl.password': password
-  }
-  
+                # Converte valores numéricos e booleanos
+                if value.lower() in ["true", "false"]:
+                    value = value.lower() == "true"
+                else:
+                    try:
+                        value = int(value)
+                    except ValueError:
+                        pass
 
+                props[key] = value
+    return props
+
+def start_kafka_producer():  
+  conf = load_properties("streaming-python-producer.properties")
   # Create Producer instance  
   producer = Producer(**conf)  
   return producer
@@ -36,12 +38,9 @@ def send_to_kafka(producer, topic, message):
 
 
 message_template_small = {
-    "EventName": "EquitiesExecutionReport",
+    "EventName": "MessageTest",
     "MsgSeqNum": 0
 }
-
-gg_template = {"table":"MYAPP1.T1","op_type":"I","op_ts":"2024-08-13 15:54:17.020636","current_ts":"2024-08-13 15:54:19.407000","pos":"00000000160000003101","after":{"A":6,"B":"Teste 6"}}
-
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
@@ -53,7 +52,7 @@ number_of_messages = 100
 start_time = time.time() 
 for count in range(number_of_messages):
     message_template_small['MsgSeqNum'] = count+1
-    json_data = json.dumps(gg_template)
+    json_data = json.dumps(message_template_small)
     send_to_kafka(producer, topic, json_data)
 
 producer.flush()
